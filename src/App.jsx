fd354558ee30f8f3e77622665744f9ed12ce6eb7
Zakir-Khan11 <xakirkjan.01@gmail.com>
2025-11-11 23:48:06 +0500

    Change1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Intro from "./components/Intro";
-// import Cards from "./components/Cards";
-// import Projects from "./components/Projects";
-// import Footer from "./components/Footer";
-// import Calculator from './components/Calculator'
+import Navbar from "./components/Navbar";
 
-// import ContactForm from './components/ContactForm';
+import Footer from "./components/Footer";
+
 import "./index.css";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -15,19 +11,11 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Weather from "./components/Weather";
 import Calculator from "./components/Calculator";
-// import Todo from './components/Todo';
-// import Login from './components/Login';
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <Intro /> */}
-      {/* <Cards /> */}
-      {/* <Projects /> */}
-      {/* <Footer /> */}
-      {/* <Calculator/> */}
-      {/* <ContactForm/> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
@@ -37,8 +25,7 @@ function App() {
         <Route path="weather" element={<Weather />} />
         <Route path="/calculator" element={<Calculator />} />
       </Routes>
-      {/* <Login /> */}
-      {/* <Todo/> */}
+      <Footer />
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+
 import Intro from '../components/Intro'
 import Cards from '../components/Cards'
 import Projects from '../components/Projects'
-import Footer from '../components/Footer'
+
 export default function Home() {
   return (
     <div>
-           <Navbar />
+
       <Intro />
       <Cards />
       <Projects />
-      <Footer />
 
     </div>
   )
